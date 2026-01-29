b786c294e2b3ea2cc97129b6bf3a81918557971d
popusdev <popusuwu@gmail.com>
2026-01-29 10:20:10 +0000

    card component

--- Components/Card.tsx
+++ Components/Card.tsx
@@ -1,30 +1,28 @@
-import { Text, View, StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
+import React, { ReactNode } from "react";
 
-export default function Index() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.card}>
-        <View style={styles.content}></View>
-        <View style={styles.footer}></View>
-      </View>
-    </View>
-  );
+export default function Card({ children }: { children: React.ReactNode }){
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.card}>
+                <View style={styles.content}>{children}</View>
+            </View>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
   container: {
     padding: 16,
     backgroundColor: 'white',
-    flex: 1
   },
   card: {
     width: "100%",
     maxWidth: 420,
-    padding: 16,
     alignSelf: "center",
     borderRadius: 16,
     backgroundColor: '#dbdbdb',
-    aspectRatio: 3 / 2,
   },
   content: {
     flex: 1
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Text, StyleSheet } from "react-native";
+import Card from '../../Components/Card';
+
+export default function Index() {
+  return (
+    <Card>
+      <Text style={{color: 'black'}}>siema</Text>
+    </Card>
+  )
+}
